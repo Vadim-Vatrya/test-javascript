6fe704bbe611f87f52b82646887c7d72ed4eb614
Vadim Vatrya <VatryaVV@gmail.com>
2022-02-04 00:06:56 +0200

    89

--- code_mu/index_88-99.js
+++ code_mu/index_88-99.js
@@ -228,7 +228,118 @@
 // };
 // console.log(result);
 
+// 91 
+// 99.1 Реализуйте функцию getOwnDivisors. Потестируйте ее работу.
+
+// function getOwnDivisors(num) {
+//    let result = [];
+//    for (let i = 1; i < num; i += 1){
+//        if (num % i === 0) {
+//            result.push(i);
+//        }
+//    }
+//     return result;
+// };
+
+// console.log(getOwnDivisors(11230));
+// console.log(getOwnDivisors(6666));
+// console.log(getOwnDivisors(220));
+// console.log(getOwnDivisors(284));
+
+// 99.2 Реализуйте функцию getSum. Потестируйте ее работу.
+
+// function getSum(arr) {
+//     let sum  = 0;
+//     for (let elem of arr) {
+//         sum += Number(elem);
+//     }
+//     return sum;
+// };
+// console.log(getSum(getOwnDivisors(220)));
+// console.log(getSum(getOwnDivisors(284)));
+
+// 99.3 Проверьте работу функции isFreindly.
+
+// function isFrendly(num1, num2) {
+// 	let sum1 = getSum(getOwnDivisors(num1));
+// 	let sum2 = getSum(getOwnDivisors(num2));
+// 	 return sum1 == num2 && sum2 == num1
+// 	// if (sum1 == num2 && sum2 == num1) {
+// 	// 	return true;
+// 	// } else {
+// 	// 	return false;
+// 	// }
+// };
+
+// console.log(isFrendly(220, 284));
+
+// 99.4 Сделайте функцию getFreindly, которая будет находить пары дружественных чисел в заданном промежутке и возвращать их 
+// в виде двухмерного массива вида следующего вида:[ [220, 284], [1184, 1210], [2620, 2924] ]. С помощью созданной функции найдите 
+// все пары дружественных чисел на промежутке от 1 до 3000.
+
+// console.log(getFreindly(1, 3000));
+
+// function getFreindly(a, b) {
+//     let arr = [];
+//     for (let i = a; i <= b; i += 1) {
+//         for (let j = i +1; j <= b; j += 1) {
+//             if (i === getSum(getOwnDivisors(j)) && j === getSum(getOwnDivisors(i))) {
+//                 arr.push([i, j]);
+//             }
+//         }
+//     }
+//     return arr;
+// }
+
+// function getSum(arr) {
+//     let sum  = 0;
+//     for (let elem of arr) {
+//         sum += Number(elem);
+//     }
+//     return sum;
+// };
+
+// function getOwnDivisors(num) {
+//        let result = [];
+//        for (let i = 1; i < num; i += 1){
+//            if (num % i === 0) {
+//                result.push(i);
+//            }
+//        }
+//         return result;
+//     };
 
 
+// 99.5 Совершенное число - целое число, равное сумме всех своих собственных делителей (то есть всех положительных делителей, 
+// отличных от самого числа). Сделайте функцию getPerfect,
+//  которая будет находить совершенные числа в заданном диапазоне. Проверьте работу функции в промежутке от 1 до 1000.
 
+// function getPerfect(a, b) {
+//     let arr = [];
+//     for (let i = a; i <= b; i += 1) {
+//         if (getSum(getOwnDivisors(i)) === i) {
+//             arr.push(i);
+//         }
+//     }
+//     return arr;
+// };
 
+// function getSum(arr) {
+//     let sum  = 0;
+//     for (let elem of arr) {
+//         sum += Number(elem);
+//     }
+//     return sum;
+// };
+
+// function getOwnDivisors(num) {
+//        let result = [];
+//        for (let i = 1; i < num; i += 1){
+//            if (num % i === 0) {
+//                result.push(i);
+//            }
+//        }
+//         return result;
+//     };
+
+//     console.log(getPerfect(1, 1000));
