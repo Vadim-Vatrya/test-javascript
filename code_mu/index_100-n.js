9c88903be56ebb8435cf42b9c92c69cdcaf72965
Vadim Vatrya <VatryaVV@gmail.com>
2022-02-04 22:27:28 +0200

    106

--- code_mu/index_100-n.js
+++ code_mu/index_100-n.js
@@ -1,7 +1,92 @@
-// 103 Сделайте функцию func, которая будет возвращать через return какую-либо строку.
+// 103.1 Сделайте функцию func, которая будет возвращать через return какую-либо строку.
 
-function func () {
-    return 'String';
-}
+// function func () {
+//     return 'String';
+// };
 
-console.log(func());
+// console.log(func);
+
+// 103.2 Выведите результат работы функции func алертом на экран.
+// func();
+  
+// 103.3 Выведите исходный код функции func алертом на экран.
+// alert(func())
+
+// 103.4 Запишите в переменную func число 123, тем самым затерев функцию из этой переменной. 
+// Выведите новое значение переменной func на экран.
+
+// func = 123;
+// console.log(func)
+
+// 103.5 Сделайте функцию func1, которая будет возвращать через return число 3.
+
+// function func1() {
+//     return 3;
+// };
+
+// console.log(func());
+
+// 103.6 Скопируйте исходный код функции func1, в переменную func2.
+
+// let func2 = func1;
+// console.log(func2());
+// console.log(func1());
+
+// function funcSum() {
+//     return func2() + func1()
+// }
+
+// console.log(funcSum())
+
+// 103.7 Выведите на экран сумму результатов работы функций func1 и func2.
+
+// function func1() {
+//     return 3;
+// };
+
+// let func2 = func1;
+// console.log(func2());
+// console.log(func1());
+
+// function funcSum() {
+//     return func2 + func1;
+// }
+
+// console.log(funcSum());
+
+// 103.7 
+// let func = function() {
+// 	return 1;
+// };
+
+// 106
+
+// 106.1 Сделайте массив arr с тремя функциями. Пусть первая возвращает через return число 1,
+//  вторая - число 2, третья - число 3.
+// let arr = [
+// 	function() {return 1},
+// 	function() {return 2},
+// 	function() {return 3},
+// ];
+//  106.2
+// console.log(arr[2]())
+// arr[2]();
+// // 106.3
+// let sum =  arr[0]() + arr[1]() + arr[2]()
+
+// console.log(sum);
+
+// 106.4 Переберите созданный вами массив arr циклом и выведите результаты работы всех функций 
+// на экран.
+
+// let arr = [
+// 	function() {return 1},
+// 	function() {return 2},
+// 	function() {return 3},
+// ];
+
+// for (let i = 0; i < arr.length; i += 1) {
+// 	alert(arr[i]());
+// };
+
+
